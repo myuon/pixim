cd6640355ed052c365408359fd22f8fefb36d66d
myuon <ioi.joi.koi.loi@gmail.com>
2024-09-21 23:56:17 +0900

    fix: set containerSize

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 
 	prevPos := fyne.NewPos(0, 0)
 
-	mainCanvas := NewMainCanvas(cimg)
+	mainCanvas := NewMainCanvas(cimg, fyne.NewSize(800, 800))
 	mainCanvas.OnMouseDown = func(e *desktop.MouseEvent, x, y int, contains bool) {
 		if mode == Move {
 			dragging = true
@@ -150,17 +150,19 @@ func main() {
 		mainCanvas,
 	)
 	mainCanvas.Refresh()
+
 	w.SetContent(content)
 	w.ShowAndRun()
 }
 
 type MainCanvas struct {
 	widget.BaseWidget
-	Image  *canvas.Image
-	Grid   *canvas.Raster
-	Widget fyne.CanvasObject
-	Ratio  *float64
-	Color  color.Color
+	Image         *canvas.Image
+	Grid          *canvas.Raster
+	Widget        fyne.CanvasObject
+	Ratio         *float64
+	Color         color.Color
+	ContainerSize fyne.Size
 
 	OnMouseDown func(*desktop.MouseEvent, int, int, bool)
 	OnMouseUp   func(*desktop.MouseEvent)
@@ -172,40 +174,41 @@ var _ desktop.Cursorable = (*MainCanvas)(nil)
 var _ desktop.Mouseable = (*MainCanvas)(nil)
 var _ desktop.Hoverable = (*MainCanvas)(nil)
 
-func NewMainCanvas(img *canvas.Image) *MainCanvas {
+func NewMainCanvas(img *canvas.Image, containerSize fyne.Size) *MainCanvas {
 	ratio := 1.0
 
 	background := widgets.NewCheckerPattern(
-		fyne.NewSize(400, 400),
+		fyne.NewSize(containerSize.Width, containerSize.Height),
 		fyne.NewSize(40, 40),
 	)
 	grid := canvas.NewRaster(func(w, h int) image.Image {
-		img := image.NewRGBA(image.Rect(0, 0, 400, 400))
+		img := image.NewRGBA(image.Rect(0, 0, int(containerSize.Width), int(containerSize.Height)))
 		if ratio < 5 {
 			return img
 		}
 
-		for i := 0; i < 400; i++ {
-			for j := 0; j < 400; j++ {
+		for i := 0; i < int(containerSize.Width); i++ {
+			for j := 0; j < int(containerSize.Height); j++ {
 				if i%int(ratio) == 0 || j%int(ratio) == 0 {
-					img.Set(i, j, color.RGBA{0, 0, 0, 0x20})
+					img.Set(i, j, color.RGBA{0xd0, 0xd0, 0xd0, 0xff})
 				}
 			}
 		}
 
 		return img
 	})
-	grid.Resize(fyne.NewSize(400, 400))
+	grid.Resize(containerSize)
 	grid.ScaleMode = canvas.ImageScalePixels
 
 	widget := container.New(&widgets.StackingLayout{}, background, img, grid)
 
 	item := &MainCanvas{
-		Image:  img,
-		Grid:   grid,
-		Widget: widget,
-		Ratio:  &ratio,
-		Color:  color.Black,
+		Image:         img,
+		Grid:          grid,
+		Widget:        widget,
+		Ratio:         &ratio,
+		Color:         color.Black,
+		ContainerSize: containerSize,
 	}
 	item.ExtendBaseWidget(item)
 
@@ -213,7 +216,7 @@ func NewMainCanvas(img *canvas.Image) *MainCanvas {
 }
 
 func (m *MainCanvas) MinSize() fyne.Size {
-	return fyne.NewSize(400, 400)
+	return m.ContainerSize
 }
 
 func (m *MainCanvas) CreateRenderer() fyne.WidgetRenderer {
