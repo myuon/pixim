03b6b005a67012d041bfcaf74296566562dab0c0
myuon <ioi.joi.koi.loi@gmail.com>
2024-09-22 15:04:56 +0900

    refactor: MainCanvas as a container

--- main.go
+++ main.go
@@ -35,26 +35,83 @@ func main() {
 	cimg := canvas.NewImageFromImage(pixImage.Image)
 	cimg.ScaleMode = canvas.ImageScalePixels
 
+	ratio := 1.0
+	containerSize := fyne.NewSize(800, 800)
+
+	background := widgets.NewCheckerPattern(
+		fyne.NewSize(containerSize.Width, containerSize.Height),
+		fyne.NewSize(40, 40),
+	)
+
+	var gridCache image.Image
+	cachedRatio := 0.0
+	grid := canvas.NewRaster(func(w, h int) image.Image {
+		if gridCache != nil && cachedRatio == ratio {
+			return gridCache
+		}
+
+		img := image.NewRGBA(image.Rect(0, 0, int(containerSize.Width), int(containerSize.Height)))
+		if ratio < 5 {
+			return img
+		}
+
+		for i := 0; i < int(containerSize.Width); i++ {
+			for j := 0; j < int(containerSize.Height); j++ {
+				if i%int(ratio) == 0 || j%int(ratio) == 0 {
+					img.Set(i, j, color.RGBA{0xd0, 0xd0, 0xd0, 0xff})
+				}
+			}
+		}
+
+		gridCache = img
+		cachedRatio = ratio
+
+		return img
+	})
+	grid.Resize(containerSize)
+	grid.ScaleMode = canvas.ImageScalePixels
+
+	imageCanvas := widgets.NewImageCanvas(pixim.NewPixImage())
+
+	imgContainer := container.New(&widgets.StackingLayout{}, imageCanvas, grid)
+	imgContainer.Resize(containerSize)
+
+	scrollContainer := container.NewScroll(imgContainer)
+	scrollContainer.Resize(containerSize)
+
+	children := container.New(
+		&widgets.StackingLayout{},
+		background,
+		scrollContainer,
+	)
+
 	mode := Move
 	dragging := false
-	dragStart := fyne.NewPos(0, 0)
-	originalPos := fyne.NewPos(0, 0)
+	// dragStart := fyne.NewPos(0, 0)
+	// originalPos := fyne.NewPos(0, 0)
 
 	prevPos := fyne.NewPos(0, 0)
 
-	mainCanvas := NewMainCanvas(cimg, fyne.NewSize(800, 800))
-	mainCanvas.OnMouseDown = func(e *desktop.MouseEvent, x, y int, contains bool) {
+	var currentColor color.Color = color.Transparent
+
+	mainCanvas := NewMainCanvas(children)
+	mainCanvas.OnMouseDown = func(e *desktop.MouseEvent) {
+		pos := e.Position
+		x := int(float64(pos.X) / ratio)
+		y := int(float64(pos.Y) / ratio)
+		contains := !(x < 0 || y < 0 || x >= imageCanvas.Image.Image.Bounds().Dx() || y >= imageCanvas.Image.Image.Bounds().Dy())
+
 		if mode == Move {
 			dragging = true
-			dragStart = e.Position
-			originalPos = cimg.Position()
+			// dragStart = e.Position
+			// originalPos = cimg.Position()
 		}
 		if mode == Magnifier {
 			if e.Button == desktop.MouseButtonPrimary {
-				*mainCanvas.Ratio *= 2
+				ratio *= 2
 				mainCanvas.Refresh()
 			} else if e.Button == desktop.MouseButtonSecondary {
-				*mainCanvas.Ratio /= 2
+				ratio /= 2
 				mainCanvas.Refresh()
 			}
 		}
@@ -63,7 +120,7 @@ func main() {
 				return
 			}
 
-			pixImage.Fill(x, y, mainCanvas.Color)
+			pixImage.Fill(x, y, currentColor)
 			mainCanvas.Refresh()
 		}
 		if mode == Pencil {
@@ -71,7 +128,7 @@ func main() {
 				return
 			}
 
-			pixImage.Image.Set(x, y, mainCanvas.Color)
+			pixImage.Image.Set(x, y, currentColor)
 			mainCanvas.Refresh()
 
 			prevPos = fyne.NewPos(float32(x), float32(y))
@@ -79,16 +136,21 @@ func main() {
 			dragging = true
 		}
 	}
-	mainCanvas.OnMouseMove = func(e *desktop.MouseEvent, x, y int, contains bool) {
+	mainCanvas.OnMouseMove = func(e *desktop.MouseEvent) {
+		pos := e.Position
+		x := int(float64(pos.X) / ratio)
+		y := int(float64(pos.Y) / ratio)
+		contains := !(x < 0 || y < 0 || x >= imageCanvas.Image.Image.Bounds().Dx() || y >= imageCanvas.Image.Image.Bounds().Dy())
+
 		if mode == Move && dragging {
-			mainCanvas.MoveImage(fyne.NewPos(float32(e.Position.X-dragStart.X)+originalPos.X, float32(e.Position.Y-dragStart.Y)+originalPos.Y))
+			// mainCanvas.MoveImage(fyne.NewPos(float32(e.Position.X-dragStart.X)+originalPos.X, float32(e.Position.Y-dragStart.Y)+originalPos.Y))
 		}
 		if mode == Pencil && dragging {
 			if !contains {
 				return
 			}
 
-			pixImage.DrawLine(int(prevPos.X), int(prevPos.Y), x, y, mainCanvas.Color)
+			pixImage.DrawLine(int(prevPos.X), int(prevPos.Y), x, y, currentColor)
 			mainCanvas.Refresh()
 
 			prevPos = fyne.NewPos(float32(x), float32(y))
@@ -97,7 +159,7 @@ func main() {
 	mainCanvas.OnMouseUp = func(e *desktop.MouseEvent) {
 		if mode == Move {
 			dragging = false
-			mainCanvas.MoveImage(fyne.NewPos(float32(e.Position.X-dragStart.X)+originalPos.X, float32(e.Position.Y-dragStart.Y)+originalPos.Y))
+			// mainCanvas.MoveImage(fyne.NewPos(float32(e.Position.X-dragStart.X)+originalPos.X, float32(e.Position.Y-dragStart.Y)+originalPos.Y))
 		}
 		if mode == Pencil {
 			dragging = false
@@ -136,7 +198,7 @@ func main() {
 									}
 								}
 
-								mainCanvas.Canvas.ReplaceImage(img)
+								imageCanvas.ReplaceImage(img)
 								mainCanvas.Refresh()
 							}, w)
 						},
@@ -201,7 +263,7 @@ func main() {
 					"Select a color",
 					"foobar",
 					func(c color.Color) {
-						mainCanvas.Color = c
+						currentColor = c
 					},
 					w,
 				).Show()
@@ -216,20 +278,11 @@ func main() {
 }
 
 type MainCanvas struct {
-	widget.BaseWidget
-	Image           *canvas.Image
-	ScrollContainer *container.Scroll
-	Grid            *canvas.Raster
-	Widget          fyne.CanvasObject
-	Ratio           *float64
-	Color           color.Color
-	ContainerSize   fyne.Size
-	ImagePosition   fyne.Position
-	Canvas          widgets.ImageCanvas
+	*fyne.Container
 
-	OnMouseDown func(*desktop.MouseEvent, int, int, bool)
+	OnMouseDown func(*desktop.MouseEvent)
 	OnMouseUp   func(*desktop.MouseEvent)
-	OnMouseMove func(*desktop.MouseEvent, int, int, bool)
+	OnMouseMove func(*desktop.MouseEvent)
 }
 
 var _ fyne.Widget = (*MainCanvas)(nil)
@@ -237,77 +290,16 @@ var _ desktop.Cursorable = (*MainCanvas)(nil)
 var _ desktop.Mouseable = (*MainCanvas)(nil)
 var _ desktop.Hoverable = (*MainCanvas)(nil)
 
-func NewMainCanvas(img *canvas.Image, containerSize fyne.Size) *MainCanvas {
-	ratio := 1.0
-
-	background := widgets.NewCheckerPattern(
-		fyne.NewSize(containerSize.Width, containerSize.Height),
-		fyne.NewSize(40, 40),
-	)
-
-	var gridCache image.Image
-	cachedRatio := 0.0
-	grid := canvas.NewRaster(func(w, h int) image.Image {
-		if gridCache != nil && cachedRatio == ratio {
-			return gridCache
-		}
-
-		img := image.NewRGBA(image.Rect(0, 0, int(containerSize.Width), int(containerSize.Height)))
-		if ratio < 5 {
-			return img
-		}
-
-		for i := 0; i < int(containerSize.Width); i++ {
-			for j := 0; j < int(containerSize.Height); j++ {
-				if i%int(ratio) == 0 || j%int(ratio) == 0 {
-					img.Set(i, j, color.RGBA{0xd0, 0xd0, 0xd0, 0xff})
-				}
-			}
-		}
-
-		gridCache = img
-		cachedRatio = ratio
-
-		return img
-	})
-	grid.Resize(containerSize)
-	grid.ScaleMode = canvas.ImageScalePixels
-
-	imageCanvas := widgets.NewImageCanvas(pixim.NewPixImage())
-
-	imgContainer := container.New(&widgets.StackingLayout{}, imageCanvas, grid)
-	imgContainer.Resize(containerSize)
-
-	scrollContainer := container.NewScroll(imgContainer)
-	scrollContainer.Resize(containerSize)
-
-	widget := container.New(
-		&widgets.StackingLayout{},
-		background,
-		scrollContainer,
-	)
-
+func NewMainCanvas(chilren *fyne.Container) *MainCanvas {
 	item := &MainCanvas{
-		Image:           img,
-		ScrollContainer: scrollContainer,
-		Grid:            grid,
-		Canvas:          *imageCanvas,
-		Widget:          widget,
-		Ratio:           &ratio,
-		Color:           color.Black,
-		ContainerSize:   containerSize,
+		Container: chilren,
 	}
-	item.ExtendBaseWidget(item)
 
 	return item
 }
 
-func (m *MainCanvas) MinSize() fyne.Size {
-	return m.ContainerSize
-}
-
 func (m *MainCanvas) CreateRenderer() fyne.WidgetRenderer {
-	return widget.NewSimpleRenderer(m.Widget)
+	return widget.NewSimpleRenderer(m.Container)
 }
 
 func (m *MainCanvas) Cursor() desktop.Cursor {
@@ -315,12 +307,7 @@ func (m *MainCanvas) Cursor() desktop.Cursor {
 }
 
 func (m *MainCanvas) MouseDown(e *desktop.MouseEvent) {
-	pos := e.Position
-	x := int(float64(pos.X) / *m.Ratio)
-	y := int(float64(pos.Y) / *m.Ratio)
-	contains := !(x < 0 || y < 0 || x >= m.Image.Image.Bounds().Dx() || y >= m.Image.Image.Bounds().Dy())
-
-	m.OnMouseDown(e, x, y, contains)
+	m.OnMouseDown(e)
 }
 
 func (m *MainCanvas) MouseUp(e *desktop.MouseEvent) {
@@ -328,12 +315,7 @@ func (m *MainCanvas) MouseUp(e *desktop.MouseEvent) {
 }
 
 func (m *MainCanvas) MouseMoved(e *desktop.MouseEvent) {
-	pos := e.Position
-	x := int(float64(pos.X) / *m.Ratio)
-	y := int(float64(pos.Y) / *m.Ratio)
-	contains := !(x < 0 || y < 0 || x >= m.Image.Image.Bounds().Dx() || y >= m.Image.Image.Bounds().Dy())
-
-	m.OnMouseMove(e, x, y, contains)
+	m.OnMouseMove(e)
 }
 
 func (m *MainCanvas) MouseIn(e *desktop.MouseEvent) {
@@ -343,12 +325,16 @@ func (m *MainCanvas) MouseOut() {
 }
 
 func (m *MainCanvas) Refresh() {
-	m.Image.SetMinSize(fyne.NewSize(float32(float64(m.Image.Image.Bounds().Dx())**m.Ratio), float32(float64(m.Image.Image.Bounds().Dy())**m.Ratio)))
-	m.Image.Resize(fyne.NewSize(float32(float64(m.Image.Image.Bounds().Dx())**m.Ratio), float32(float64(m.Image.Image.Bounds().Dy())**m.Ratio)))
-	m.Image.Refresh()
-	m.Widget.Refresh()
+	m.Container.Refresh()
 }
 
-func (m *MainCanvas) MoveImage(pos fyne.Position) {
-	m.ImagePosition = pos
-}
+// func (m *MainCanvas) Refresh() {
+// 	m.Image.SetMinSize(fyne.NewSize(float32(float64(m.Image.Image.Bounds().Dx())**m.Ratio), float32(float64(m.Image.Image.Bounds().Dy())**m.Ratio)))
+// 	m.Image.Resize(fyne.NewSize(float32(float64(m.Image.Image.Bounds().Dx())**m.Ratio), float32(float64(m.Image.Image.Bounds().Dy())**m.Ratio)))
+// 	m.Image.Refresh()
+// 	m.Widget.Refresh()
+// }
+
+// func (m *MainCanvas) MoveImage(pos fyne.Position) {
+// 	m.ImagePosition = pos
+// }
